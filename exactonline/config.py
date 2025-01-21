ce9be407fe586ede71aef9905d138368fba2191c
Joost Hietbrink <joost@joopp.com>
2025-01-21 17:09:02 +0100

    Working mypy.

--- exactonline/config.py
+++ exactonline/config.py
@@ -1,4 +1,6 @@
-CACHE = {}
+from typing import Dict, Any
+
+CACHE: Dict[str, Any] = {}
 BASE_URL = 'https://start.exactonline.be/api/v1'
 AUTH_URL = 'https://start.exactonline.be/api/oauth2/auth'
 ACCESS_TOKEN_URL = 'https://start.exactonline.be/api/oauth2/token'
